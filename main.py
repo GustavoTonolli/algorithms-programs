4fc1b5fb6693706f303b6ec8a7490f558615ba37
gustavotonolli <gustavo.tonolli@hotmail.com>
2023-09-25 21:05:58 -0300

    exercises

---
+++ main.py
@@ -0,0 +1,44 @@
+
+# nota1 = float(input('Nota1: '))
+# nota2 = float(input('Nota2: '))
+# nota3 = float(input('Nota3: '))
+
+# media = ((nota1 * 2) + (nota2 * 3) + (nota3 * 5)) / 10
+
+# print(f'A média é do aluno é: {media}')
+
+# base = int(input('Base: '))
+# expoente = int(input('Expoente: '))
+
+# resultado = base ** expoente
+
+# print(resultado)\
+    
+# grausC = float(input('Digite os graus: '))
+
+# F = ((9 * grausC) + 160) / 5
+
+# print(f'{grausC}C em fahrenheit é: {F}F')
+
+# n1 = int(input('N1: '))
+# n2 = int(input('N2: '))
+
+# if   n1  > n2:
+#     print(n1)
+# elif n1 == n2:
+#     print('numeros iguais')
+# else:
+#     print(n2)
+    
+# valor   = float(input('Qual é o valor: '))
+# parcelas = int(input('Qual o número de parcelas: '))
+# salario = float(input('Qual o valor do salário: '))
+
+# valorParcelas = valor / parcelas
+
+# if valorParcelas > salario:
+#     print(f'Empréstimo não foi aprovado! Parcela maior que o salário. Valor das parcelas: {valorParcelas} Salário: {salario}')
+# else:
+#     print(f'O empréstimo foi aprovado! Valor das parcelas: {valorParcelas} Salário: {salario}')
+
+    
